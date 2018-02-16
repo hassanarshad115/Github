762897c9bcbb25c005720400d367142963a6a495
HASSAN ARSHAD <hassanarshad115@gmail.com>
2018-02-16 07:26:01 -0800

    send to github

--- Github/Form1.Designer.cs
+++ Github/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 27);
+            this.label1.Location = new System.Drawing.Point(43, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
